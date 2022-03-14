aca0575b122bb0741a2a1466541b6ec85007388e
40923151 <40923151@gm.nfu.edu.tw>
2022-03-14 17:18:26 +0800

    NX1980 bat_alter

---
+++ wink/NX1980/NX1980.js
@@ -0,0 +1,22 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 708, y: 177, width: 60, height: 24 }, target: 3 },
+    ],
+    "39": [
+      { type: "gotoframe", rect: { x: 809, y: 194, width: 60, height: 24 }, target: 40 },
+      { type: "gotoframe", rect: { x: 717, y: 194, width: 60, height: 24 }, target: 2 },
+    ],
+    "44": [
+      { type: "gotoframe", rect: { x: 809, y: 194, width: 60, height: 24 }, target: 45 },
+      { type: "gotoframe", rect: { x: 717, y: 194, width: 60, height: 24 }, target: 39 },
+    ],
+    "57": [
+      { type: "gotoframe", rect: { x: 695, y: 209, width: 60, height: 24 }, target: 44 },
+    ],
+  },
+};
