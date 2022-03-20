59de5f2d822f4faeb83a810730de074cf7eb97d0
40923151 <40923151@gm.nfu.edu.tw>
2022-03-20 16:25:18 +0800

    online_presentation

---
+++ wink/online_presentation/online_presentation.js
@@ -0,0 +1,34 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 747, y: 451, width: 60, height: 24 }, target: 3 },
+    ],
+    "7": [
+      { type: "gotoframe", rect: { x: 930, y: 430, width: 60, height: 24 }, target: 8 },
+      { type: "gotoframe", rect: { x: 837, y: 430, width: 60, height: 24 }, target: 2 },
+    ],
+    "12": [
+      { type: "gotoframe", rect: { x: 636, y: 579, width: 60, height: 24 }, target: 13 },
+      { type: "gotoframe", rect: { x: 543, y: 579, width: 60, height: 24 }, target: 7 },
+    ],
+    "17": [
+      { type: "gotoframe", rect: { x: 736, y: 374, width: 60, height: 24 }, target: 12 },
+      { type: "gotoframe", rect: { x: 829, y: 374, width: 60, height: 24 }, target: 18 },
+    ],
+    "22": [
+      { type: "gotoframe", rect: { x: 317, y: 478, width: 60, height: 24 }, target: 23 },
+      { type: "gotoframe", rect: { x: 224, y: 478, width: 60, height: 24 }, target: 17 },
+    ],
+    "39": [
+      { type: "gotoframe", rect: { x: 421, y: 464, width: 60, height: 24 }, target: 40 },
+      { type: "gotoframe", rect: { x: 328, y: 464, width: 60, height: 24 }, target: 22 },
+    ],
+    "69": [
+      { type: "gotoframe", rect: { x: 529, y: 303, width: 60, height: 24 }, target: 39 },
+    ],
+  },
+};
